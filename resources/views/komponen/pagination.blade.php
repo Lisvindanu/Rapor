ca47de379f8e1d068388fa6131cec9e64511f588
githubna-ilham <ilham.anugrah@unpas.ac.id>
2024-04-20 22:09:51 +0700

    pegawai

--- resources/views/komponen/pagination.blade.php
+++ resources/views/komponen/pagination.blade.php
@@ -9,11 +9,15 @@
     <ul class="pagination justify-content-center">
         <!-- Tombol Previous -->
         <li class="page-item {{ $data->currentPage() == 1 ? 'disabled' : '' }}">
-            <a href="{{ $data->url(1) }}" class="page-link">Previous</a>
+            <a href="{{ $data->url(max(1, $data->currentPage() - 1)) }}" class="page-link">Previous</a>
         </li>
 
         <!-- Nomor Halaman -->
-        @for ($i = 1; $i <= $data->lastPage(); $i++)
+        @php
+            $startPage = max(1, min($data->lastPage() - 9, $data->currentPage() - 4));
+            $endPage = min($startPage + 9, $data->lastPage());
+        @endphp
+        @for ($i = $startPage; $i <= $endPage; $i++)
             <li class="page-item {{ $data->currentPage() == $i ? 'active' : '' }}">
                 <a href="{{ $data->url($i) }}" class="page-link">{{ $i }}</a>
             </li>
@@ -21,7 +25,7 @@
 
         <!-- Tombol Next -->
         <li class="page-item {{ $data->currentPage() == $data->lastPage() ? 'disabled' : '' }}">
-            <a href="{{ $data->url($data->currentPage() + 1) }}" class="page-link">Next</a>
+            <a href="{{ $data->url(min($data->lastPage(), $data->currentPage() + 1)) }}" class="page-link">Next</a>
         </li>
     </ul>
 </div>

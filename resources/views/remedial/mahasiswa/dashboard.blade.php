89d3319338bf57c21ac8e957329688f00912d68d
githubna-ilham <ilham.anugrah@unpas.ac.id>
2025-03-01 22:02:17 +0700

    fix return

--- resources/views/remedial/mahasiswa/dashboard.blade.php
+++ resources/views/remedial/mahasiswa/dashboard.blade.php
@@ -111,7 +111,9 @@
                                     <div class="d-grid gap-2 d-md-flex justify-content-md-end"">
                                         @if (
                                             $data_ajuan->count() == 0 &&
-                                                \Carbon\Carbon::now()->gte($periodeTerpilih->tanggal_mulai))
+                                                \Carbon\Carbon::now()->gte($periodeTerpilih->tanggal_mulai) &&
+                                                \Carbon\Carbon::now()->lte($periodeTerpilih->tanggal_selesai) &&
+                                                $periodeTerpilih->is_aktif)
 
                                                 <!-- $data_ajuan->count() == 0 &&
                                                 \Carbon\Carbon::now()->gte($periodeTerpilih->tanggal_mulai) &&

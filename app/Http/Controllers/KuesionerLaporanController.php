64af076bd68feb9f0567dd35a1b90e9426dc620e
githubna-ilham <ilham.anugrah@unpas.ac.id>
2025-06-09 17:12:23 +0700

    rekap kuesioner

--- app/Http/Controllers/KuesionerLaporanController.php
+++ app/Http/Controllers/KuesionerLaporanController.php
@@ -168,7 +168,7 @@ class KuesionerLaporanController extends Controller
         $pdf = PDF::loadView('pdf.kuesioner-dp3', [
                 'result' => $grouped,
             ]);
-        $pdf->setPaper('A4', 'landscape');
+        $pdf->setPaper('A4', 'potrait');
         return $pdf->download('laporan-kuesioner-sdm.pdf');
         
         // return response()->json($grouped, 200, [], JSON_PRETTY_PRINT);
